fe8cbb7e0eba398473deb6c22ccf2c056964e251
jihye_Park <sconenomad@gmail.com>
2023-01-16 22:43:17 +0900

    feat: graphql 세팅

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,21 @@
+import { useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import './index.scss'
 
 // next가 SSR하기 위해 필요한 컴포넌트
-const App = ({ Component, pageProps }) => <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+    const clientRef = useRef(null)
+    const getClient = () => {
+        if (!clientRef.current) clientRef.current = new QueryClient()
+        return clientRef.current
+    }
+    return (
+        <QueryClientProvider client={getClient()}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    )
+}
+
 
 App.getInitialProps = async ({ ctx, Component }) => {
     const pageProps = await Component.getInitialProps?.(ctx)
